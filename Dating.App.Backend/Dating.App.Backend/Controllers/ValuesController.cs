4f5dc6b9a314e42f47979cae626df626bbd39798
boeing666 <archil.zivzivadze@gmail.com>
2020-04-14 22:24:21 +0400

    initial Commit

---
+++ Dating.App.Backend/Dating.App.Backend/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dating.App.Backend.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dating.App.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(
+                val => val.Id == id);
+            return Ok(value);
+
+        }
+
+    }
+}
